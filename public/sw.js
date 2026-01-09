2a0ffdac6ce3596145efee74ffe5ff1d95ee9f74
Kelvin Velasquez <112011775+kelvinvelasquez-SDE@users.noreply.github.com>
2026-01-08 20:31:53 -0600

    Deploy public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'turnos-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
